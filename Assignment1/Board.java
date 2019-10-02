2a1589f2edb9314f1da88c42ff9244215b14c531
zuercher8353 <zuercher8353@gmail.com>
2019-10-02 20:31:50 +0200

    Board Class added

--- Assignment1/Board.java
+++ Assignment1/Board.java
@@ -1,5 +1,19 @@
 public class Board {
-        int boardsize;
+        private int boardsize;
+        private int []snake;
+        private int []ladder;
+
+        public Board(int boardsize, int[] snake, int []ladder) {
+        this.boardsize = boardsize;
+        this.snake = snake;
+        this.ladder = ladder;
+        }
+
+        static void initsquare(){
+            //ruft init von square auf und gibt boardsize, snake und ladder weiter
+        }
+
+
 
 
 }
