756d577fd5d691f4b8f6adb507e5c81e216bde6f
Janosch <janosch.baltensperger@gmail.com>
2019-10-02 15:56:26 +0200

    First Hello World

---
+++ Assignment1/BoardGame.java
@@ -0,0 +1,11 @@
+public class BoardGame {
+
+
+
+
+        public static void main(String[] args) {
+            // Prints "Hello, World" to the terminal window.
+            System.out.println("Hello, World");
+        }
+
+}
