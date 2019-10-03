b6c612a138c8368f18235c540cb3762476e87042
Janosch <janosch.baltensperger@gmail.com>
2019-10-03 11:59:50 +0200

    Player Setup

--- Assignment1/BoardGame.java
+++ Assignment1/BoardGame.java
@@ -1,27 +1,40 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class BoardGame {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            int playerCount = 0;
+        int playerCount = 0;
 
-            while(playerCount < 2 || playerCount > 4) {
-
-                System.out.println("Your number of players: ");
-                Scanner in = new Scanner(System.in);
-                playerCount = in.nextInt();
-            }
-
-
-            Player[] players = new Player[playerCount];
-            for (int i = 0; i < playerCount; i++) {
-
-                System.out.println("Name of player Nr. "+ (i+1)+" :" );
-                Scanner input = new Scanner(System.in);
-                String playerName = input.nextLine();
-                players[i] = new Player(playerName);
+        boolean correctPlayerCount = false;
+        while (!correctPlayerCount) {
+            try {
+                System.out.println("Choose between 2-4 persons");
+                System.out.println("How many players want to play? ");
+                Scanner playerCount_players = new Scanner(System.in);
+                playerCount = playerCount_players.nextInt();
+                if (playerCount <= 4 && playerCount >= 2) {
+                    correctPlayerCount = true;
+                } else {
+                    System.out.println("Choose 2, 3 or 4 players");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input");
             }
         }
+        System.out.println("Amount of players: " + playerCount);
+
+        Player[] players = new Player[playerCount];
+        for (
+                int i = 0;
+                i < playerCount; i++) {
+
+            System.out.println("Name of player Nr. " + (i + 1) + " :");
+            Scanner input = new Scanner(System.in);
+            String playerName = input.nextLine();
+            players[i] = new Player(playerName);
+        }
+    }
 }
 
 
