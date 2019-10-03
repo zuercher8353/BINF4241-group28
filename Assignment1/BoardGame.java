b7645beb2b772fb37d8bc4894cb967029850ff42
Janosch <janosch.baltensperger@gmail.com>
2019-10-03 10:21:39 +0200

    Player class startet

--- Assignment1/BoardGame.java
+++ Assignment1/BoardGame.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class BoardGame {
         public static void main(String[] args) {
-            Player Player1 = new Player("Janosch");
-            Player Player2 = new Player("Jonas");
+
+
+            Scanner in = new Scanner(System.in);
+            int playerCount = in.nextInt();
+            System.out.println("Your number of players: "+ playerCount);
+
+            Player players[playerCount];
+            for (int i = 0; i <= playerCount; i++) {
+
+                System.out.println("Your number of players: "+ playerCount);
+                Scanner input = new Scanner(System.in);
+                String playerName = input.nextLine();
+                players[i] = new Player(playerName);
+            }
         }
 }
+
+
