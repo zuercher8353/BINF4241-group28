5989856a99f99a1eb599448582609b2c3df690b3
Ülkü Karagöz <uelkue.karagoez@uzh.ch>
2019-10-16 14:53:44 +0200

    check input name

--- Assignment2/Game.java
+++ Assignment2/Game.java
@@ -23,13 +23,20 @@ public class Game {
         int nrOfPlayers = 0;
 
         while (nrOfPlayers < 2) {
-            Scanner inputPlayer = new Scanner(System.in);  // Create a Scanner object
-            System.out.print("Enter Name: ");
-            String inputPlayerName = inputPlayer.nextLine(); // Read Input
-            Player player = new Player(inputPlayerName); // Create New Player
-            players.add(player);
+            try {
+                Scanner inputPlayer = new Scanner(System.in);  // Create a Scanner object
+                System.out.print("Enter Name: ");
+                String inputPlayerName = inputPlayer.nextLine(); // Read Input
+                if (!inputPlayerName.isEmpty()) {
+                    Player player = new Player(inputPlayerName); // Create New Player
+                    players.add(player);
 
-            nrOfPlayers += 1;
+                    nrOfPlayers += 1;
+                }
+            }
+            catch (Exception e){
+                System.out.println("Invalid input");
+            }
         }
         System.out.print('\n');
     }
