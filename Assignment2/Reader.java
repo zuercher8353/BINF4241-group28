a0eb9ff2814c499ad12e6b3891bd28800450fd72
Ülkü Karagöz <uelkue.karagoez@uzh.ch>
2019-10-16 14:46:11 +0200

    check input

--- Assignment2/Reader.java
+++ Assignment2/Reader.java
@@ -1,12 +1,14 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Reader {
     private enum FigureSTRs {P, T, N, Q, K}
 
     public void readMove() {
-        int[] moveArray = new int[5];
+        int[] moveArray = new int[4];
         Boolean validInput = false;
+        char[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
         while (!validInput)
 
@@ -18,35 +20,38 @@ public class Reader {
 
                 //validate Input
                 char figure = moveInput.charAt(0);
-                int startX = moveInput.charAt(1);
+                char startX = moveInput.charAt(1);
                 int startY = Character.getNumericValue(moveInput.charAt(2));
-                int endX = Character.getNumericValue(moveInput.charAt(3));
+                char endX = moveInput.charAt(3);
                 int endY = Character.getNumericValue(moveInput.charAt(4));
 
-                //check if Indices are in Range
-                if (startX >= 0 && startX < 8) {
-                    moveArray[0] = startX;
-                } else {
-                    throw new ArithmeticException("Index out of Range");
+                //check empty input
+                if (!moveInput.isEmpty()) {
+                    //check if Indices are in Range
+                    if (Arrays.asList(chars).contains(startX)) {
+                        moveArray[0] = startX;
+                    } else {
+                        throw new ArithmeticException("Index out of Range");
+                    }
+
+                    if (startY >= 0 && startY < 8) {
+                        moveArray[1] = startY;
+                    } else {
+                        throw new ArithmeticException("Index out of Range");
+                    }
+                    if (Arrays.asList(chars).contains(endX)) {
+                        moveArray[2] = endX;
+                    } else {
+                        throw new ArithmeticException("Index out of Range");
+                    }
+                    if (endY >= 0 && endY < 8) {
+                        moveArray[3] = endY;
+                    } else {
+                        throw new ArithmeticException("Index out of Range");
+                    }
+                    validInput = true;
                 }
 
-                if (startY >= 0 && startY < 8) {
-                    moveArray[1] = startY;
-                } else {
-                    throw new ArithmeticException("Index out of Range");
-                }
-                if (endX >= 0 && endX < 8) {
-                    moveArray[2] = endX;
-                } else {
-                    throw new ArithmeticException("Index out of Range");
-                }
-                if (endY >= 0 && endY < 8) {
-                    moveArray[3] = endY;
-                } else {
-                    throw new ArithmeticException("Index out of Range");
-                }
-
-                validInput = true;
 
             } catch (Exception e) {
                 System.out.print("Invalid Input!");
