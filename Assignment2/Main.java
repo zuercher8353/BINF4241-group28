da6a1f88679e206811802fc1e4404f0629f9fce3
zuercher8353 <zuercher8353@gmail.com>
2019-10-19 15:54:27 +0200

    Merged changes from Jonas 3 to master

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+
         Board board = new Board();
         Players players = new Players();
         Reader reader = new Reader();
