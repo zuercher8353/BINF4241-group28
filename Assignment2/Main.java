8dd2419345789e9d3240d0c7051dbebfa7d91709
zuercher8353 <45430834+zuercher8353@users.noreply.github.com>
2019-10-20 10:18:24 +0200

    Merged Jonas3 to master

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-
         Board board = new Board();
         Players players = new Players();
         Reader reader = new Reader();
