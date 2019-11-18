689867813d149e367903b088bab8f55713880bcd
Janosch <janosch.baltensperger@gmail.com>
2019-11-18 14:04:30 +0100

    included Iterator changed whole main and board to encapsulate players list

--- Assignment4/Smartphone.java
+++ Assignment4/Smartphone.java
@@ -13,6 +13,7 @@ public class Smartphone {
 
         Oven oven = new Oven();
         OvenCommandOn ovenCommandOn = new OvenCommandOn(oven);
+
         Microwave microwave = new Microwave();
         //Dishwasher dishwasher;
         //WashingMachine washingmachine;
