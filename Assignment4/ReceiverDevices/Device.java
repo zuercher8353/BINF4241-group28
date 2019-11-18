135aae90b7fd08b26524f912d78140311395f155
Ülkü Karagöz <uelkue.karagoez@uzh.ch>
2019-11-18 14:10:07 +0100

    merged master into Ülkü

--- Assignment4/ReceiverDevices/Device.java
+++ Assignment4/ReceiverDevices/Device.java
@@ -3,6 +3,21 @@ package ReceiverDevices;
 public abstract class Device  {
 
 
+    private enum DeviceCommands{}
+
+    private enum DeviceStates{}
+
+
+
+
+    public void printDeviceMenu(){
+        for (DeviceCommands commands : DeviceCommands.values()) {
+            System.out.println(commands);
+        }
+    }
+
+
+
     public void turnOn() {
         //new Thread
         //start Thread
