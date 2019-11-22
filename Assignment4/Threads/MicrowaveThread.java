2b85ccf09eb7deb00ea4d211fac95ceb61f71ff2
zuercher8353 <45430834+zuercher8353@users.noreply.github.com>
2019-11-22 11:33:28 +0100

    Fixed washingMachine

--- Assignment4/Threads/MicrowaveThread.java
+++ Assignment4/Threads/MicrowaveThread.java
@@ -17,7 +17,7 @@ public class MicrowaveThread implements Runnable {
     public void run() {
         while (run) {
             try {
-                Thread.sleep(1);
+                Thread.sleep(bakingTime);
                 microwave.setEnded();
             } catch (InterruptedException e) {
                 e.printStackTrace();
