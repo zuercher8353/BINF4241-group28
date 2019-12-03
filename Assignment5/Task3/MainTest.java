55e0743089338e1208aba76730d2a8717afeeea3
Janosch <janosch.baltensperger@gmail.com>
2019-12-03 15:09:57 +0100

    Tests for Player and Board

--- Assignment5/Task3/MainTest.java
+++ Assignment5/Task3/MainTest.java
@@ -1,9 +1,12 @@
-
+/**
+Testing the functionality of the Main
+ */
 public class MainTest(){
 
 /**
  * Test weather checkSyntaxInput return the same String if correct input or 'error' else
  */
+@Test
 public void testSyntaxValidation(){
         Assert.assertEquals("Should return same String", Main.checkSyntaxInput("green7","green7"));
         Assert.assertEquals("Should return error on wrong input", Main.checkSyntaxInput("green9","error"));
